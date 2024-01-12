aec45498c0da94fb2afcefa99b0d2de6dbd0425e
PAO <natthachai_bubpa@cmu.ac.th>
2024-01-12 12:58:28 +0700

    ????????????

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,17 +3,43 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+		
+    int i, key, j;
+    for (i = 1; i < N; i++) {
+        key = d[i];
+        j = i - 1;
+ 
+        
+        while (j >= 0 && d[j] < key) {
+            d[j + 1] = d[j];
+            j = j - 1;
+        }
+        d[j + 1] = key;
+		cout<<"Pass "<<i<<":";
+		int b=0;
+			while(b<N)
+			{
+				cout<<d[b];
+				if(b!=N-1){
+				    cout <<" ";
+				}
+				b++;
+			}
+			cout<<endl;
+    }
+	
 
 }
 
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	int a[11] = {12,25,30,44,2,0,4,7,55,25,50};
 	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	for(int i = 0; i < 11; i++) cout << a[i] << " ";
 	cout << "\n\n";
 	
-	insertionSort(a,10);
+	insertionSort(a,11);
 	
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for(int i = 0; i < 11; i++) cout << a[i] << " ";	
 }
+
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,50 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[N][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row " << i+1 <<": ";
+		for(int j=0 ; j<N;j++){
+			cin >> A[i][j] ;
+			
+		}
+		
+	}
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+
+	for(int i=0;i<N;i++){
+		
+		for(int j =0 ; j<N;j++){
+			 b[i][j] = 0;
+			
+		}
+		
+	}
+	for(int i=1;i<N-1;i++){
+		
+		for(int j =1 ; j<N-1;j++){
+			if(a[i+1][j] <= a[i][j] && a[i][j+1] <= a[i][j] && a[i-1][j] <= a[i][j] && a[i][j-1] <= a[i][j] ){
+				b[i][j] = 1;
+			}
+			else{
+				b[i][j] = 0;
+			}
+		}
+	}
+}
+
+
+void showMatrix(const bool b[][N]){
+	for(int i=0;i<N;i++){
+		
+		for(int j =0 ; j<N;j++){
+			cout << b[i][j] << " ";
+			
+		}
+		cout << endl;
+	}
+
+}
